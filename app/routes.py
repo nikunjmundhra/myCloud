bc364c16d08430f3bdc033d201124071f189ed72
nikunjmundhra <jarry2530@gmail.com>
2026-07-21 21:55:24 +0530

    Implement basic file upload functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, request
 from app import app
+from werkzeug.utils import secure_filename
+import os
 
 @app.route("/",methods =["GET","POST"])
 def home():
@@ -14,3 +16,17 @@ def home():
 @app.route("/login")
 def login():
     return render_template("login.html",title ="Login")
+
+@app.route("/upload" , methods =["GET","POST"])
+def upload():
+     
+    if request.method=="POST":
+        file = request.files["file"]
+        filename=secure_filename(file.filename)
+        file.save(f"uploads/{filename}")
+        print(f"Saved file : {filename}")
+
+    return render_template(
+        "upload.html",
+        title="upload"
+    )
